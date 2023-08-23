e2c693aacb00c581d8f44b66337f2ef956215146
Isabelle <112515770+isabellerutherford@users.noreply.github.com>
2023-08-23 09:37:55 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1u14OG0JniNwE0yAke6WZ1fkI9B1LXzTQSGbaibnjNwI/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1u14OG0JniNwE0yAke6WZ1fkI9B1LXzTQSGbaibnjNwI/edit#gid=1033046795';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
